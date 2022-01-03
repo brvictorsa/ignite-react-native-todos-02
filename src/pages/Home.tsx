dc6d21980e7afddc0f767b0af6adc644a7404956
Victor Santos <victorsa.mg@gmail.com>
2022-01-03 13:53:40 -0300

    initial project code

---
+++ src/pages/Home.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Header } from '../components/Header';
+import { Task, TasksList } from '../components/TasksList';
+import { TodoInput } from '../components/TodoInput';
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleAddTask(newTaskTitle: string) {
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(prev => [...prev, newTask]);
+  }
+
+  function handleToggleTaskDone(id: number) {
+    setTasks(prev => prev.map(task => (
+      {
+        id: task.id,
+        title: task.title,
+        done: task.id === id ? !task.done : task.done
+      })
+    ));
+  }
+
+  function handleRemoveTask(id: number) {
+    setTasks(prev => prev.filter(task => task.id !== id));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <TasksList
+        tasks={tasks}
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB'
+  }
+})
